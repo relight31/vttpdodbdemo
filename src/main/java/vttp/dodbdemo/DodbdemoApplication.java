11b484da19e7ab1c22369847e34d620b0d4e920b
relight31 <halfbcsina@yahoo.com.sg>
2022-04-06 13:01:23 +0800

    initial commit

---
+++ src/main/java/vttp/dodbdemo/DodbdemoApplication.java
@@ -0,0 +1,13 @@
+package vttp.dodbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodbdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodbdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/dodbdemo/DodbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.dodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
